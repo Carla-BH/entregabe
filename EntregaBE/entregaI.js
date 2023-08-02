b32c844b59e0f057ca61e963aebfa9771ffa4d09
Carla-BH <115728188+Carla-BH@users.noreply.github.com>
2023-08-01 22:18:56 -0300

    Update entregaI.js
    Se realizan cambios

--- EntregaBE/entregaI.js
+++ EntregaBE/entregaI.js
@@ -1,25 +1,25 @@
 // Importamos el módulo fs
-const fs = require('fs');
+const fs = require('fs')
 
 // Definimos la clase Product
 class Product {
   constructor(title, description, price, thumbnail, code, stock) {
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.thumbnail = thumbnail;
+    this.title = title
+    this.description = description
+    this.price = price
+    this.thumbnail = thumbnail
     this.code = code;
-    this.stock = stock;
+    this.stock = stock
   }
 }
 
 // Definimos la clase ProductManager
 class ProductManager {
   constructor(fileName) {
-    this.fileName = fileName;
-    this.products = [];
-    this.id = 1;
-    this.loadProducts();
+    this.fileName = fileName
+    this.products = []
+    this.id = 1
+    this.loadProducts()
   }
 
   addProduct(product) {
@@ -32,46 +32,46 @@ class ProductManager {
       !product.code ||
       !product.stock
     ) {
-      throw new Error('All fields are required');
+      throw new Error('All fields are required')
     }
 
     // Validamos que no haya otro producto con el mismo código
     if (this.products.some((p) => p.code === product.code)) {
-      throw new Error('Code already exists');
+      throw new Error('Code already exists')
     }
 
     // Asignamos el id al producto y lo incrementamos
-    product.id = this.id;
-    this.id++;
+    product.id = this.id
+    this.id++
 
     // Agregamos el producto al arreglo
-    this.products.push(product);
+    this.products.push(product)
 
     // Guardamos los productos en el archivo
-    this.saveProducts();
+    this.saveProducts()
   }
 
   getProducts() {
-    return this.products;
+    return this.products
   }
 
   getProductById(id) {
     // Buscamos el producto por id
-    const product = this.products.find((p) => p.id === id);
+    const product = this.products.find((p) => p.id === id)
 
     // Si lo encontramos, lo devolvemos
     if (product) {
-      return product;
+      return product
     }
 
     // Si no lo encontramos, mostramos un error
-    console.error('Not found');
+    console.error('Not found')
   }
 
   loadProducts() {
     // Leemos el archivo de forma sincrónica
     try {
-      const data = fs.readFileSync(this.fileName, 'utf-8');
+      const data = fs.readFileSync(this.fileName, 'utf-8')
 
       // Si el archivo tiene contenido, lo parseamos como JSON
       if (data) {
@@ -90,31 +90,31 @@ class ProductManager {
           );
 
           // Le asignamos el id que tenía en el archivo
-          newProduct.id = product.id;
+          newProduct.id = product.id
 
           // Agregamos el producto al arreglo
-          this.products.push(newProduct);
+          this.products.push(newProduct)
 
           // Actualizamos el valor del id con el mayor id encontrado más 1
           if (product.id >= this.id) {
-            this.id = product.id + 1;
+            this.id = product.id + 1
           }
         }
       }
     } catch (error) {
       // Si el archivo no existe, lo creamos vacío
-      fs.writeFileSync(this.fileName, '');
+      fs.writeFileSync(this.fileName, '')
     }
   }
 
   saveProducts() {
     // Convertimos el arreglo de productos en una cadena JSON
-    const data = JSON.stringify(this.products, null, 2);
+    const data = JSON.stringify(this.products, null, 2)
 
     // Escribimos la cadena en el archivo de forma sincrónica
-    fs.writeFileSync(this.fileName, data);
+    fs.writeFileSync(this.fileName, data)
   }
 }
 
 // Exportamos las clases para poder usarlas desde otro archivo
-module.exports = { Product, ProductManager };
+module.exports = { Product, ProductManager }
